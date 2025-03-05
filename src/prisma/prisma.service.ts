8d7e9e4a54bcc5b33d81ccaf4592593a2dc6b7d6
Tiago Borges <103573703+TFX19@users.noreply.github.com>
2025-03-05 19:37:04 +0000

    Created patch for users, created some e2e tests for signup, signin and user. Learned how to use a testdb with docker

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,13 +4,21 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor(config: ConfigService) {
-        super({
-            datasources: {
-                db: {
-                    url:config.get('DATABASE_URL'),
-                },
-            },
-        });
-    }
+  constructor(config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
+  }
+
+  cleanDb() {
+    //obriga a que o prisma siga esta ordem
+    return this.$transaction([
+      this.bookmark.deleteMany(),
+      this.user.deleteMany(),
+    ]);
+  }
 }
